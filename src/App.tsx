a0adb43c9cb1ffb0619dc46051ce671b04ab77ab
kidow <wcgo2ling@gmail.com>
2022-04-30 21:38:24 +0900

    파일 정리

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react'
+
+export interface Props {}
+interface State {}
+
+const App: FC<Props> = () => {
+  return <div className="container mx-auto">App</div>
+}
+
+export default App
