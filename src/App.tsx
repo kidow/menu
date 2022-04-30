3c3bec21d8d82c7ea392cb792ac4d8a2a12bf0f2
kidow <wcgo2ling@gmail.com>
2022-04-30 22:00:30 +0900

    logo 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ export interface Props {}
 interface State {}
 
 const App: FC<Props> = () => {
-  return <div className="container mx-auto">App</div>
+  return (
+    <div className="container max-w-4xl pt-10 pb-20 mx-auto">
+      <div>
+        <img src="/kidow-menu.svg" alt="" className="pl-6 h-7 sm:pl-0" />
+      </div>
+    </div>
+  )
 }
 
 export default App
